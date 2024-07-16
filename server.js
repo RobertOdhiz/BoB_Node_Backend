d67a274ae74440dd12dc89902cb72a24fc2e5c25
Robert Odhiambo <odhizrober254@gmail.com>
2024-07-16 15:39:27 +0300

    Initialised Express server: server.js - Contains express server initialization .gitignore - Removes node_modules from git initialisation

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.status(200).json({ 'status': 'OK' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
