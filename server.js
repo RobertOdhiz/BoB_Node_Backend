2f2cc8fb6bb8b3acfcca050e478caa76b32bd2fe
Robert Odhiambo <odhizrober254@gmail.com>
2024-08-07 17:56:54 +0300

    Updated CORS'

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-// app.js or index.js
 const express = require('express');
 const routes = require('./Routes');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Use the routes defined in routes.js
+app.use(cors());
+
 app.use('/', routes);
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
